09a8cb186ef9f94612243c1815619cfb1736388e
Sunstrike <sunstrike@azurenode.net>
2013-09-21 14:13:03 +0100

    Now with random messages

--- kalen.rb
+++ kalen.rb
@@ -10,11 +10,48 @@ Daemons.run_proc('kalen.rb') do
   require 'ipaddr'
   require 'multi_json'
   require 'net/http'
+  require 'googl'
 
 # Listener
 
   class POSTListener
     include Cinch::Plugin
+
+    @@start_messages = ["\#@build@: On your marks, get set, build!",
+                        "\#@build@: Building... with blocks.",
+                        "\#@build@: Starting build. If it's broken, blame the person sitting next to me.",
+                        "\#@build@: <press any key to build>"]
+    @@success_messages = ["@project@ (@channel@): All your build @version@ are belong to @link@",
+                          "@project@ (@channel@): I'MA FIRING MY LAZOR!!! @version@ @link@",
+                          "@project@ (@channel@): Build @version@ completed. What else did you want? @link@",
+                          "@project@ (@channel@): Build @version@: Buried treasure ahoy! Set sail for adventure! @link@",
+                          "@project@ (@channel@): Build @version@: Arrr.... We got that ticking gator @link@",
+                          "@project@ (@channel@): My little @version@, my little @link@",
+                          "@project@ (@channel@): We've got high @version@, we've got high @link@",
+                          "@project@ (@channel@): Build @version@ feels pretty good! @link@",
+                          "@project@ (@channel@): Build @version@ has high hopes for the future. @link@",
+                          "@project@ (@channel@): @link@. Oops, shouldn't that have been for @version@?",
+                          "@project@ (@channel@): Build @version@. Heyo! @link@",
+                          "@project@ (@channel@): Gardevoir is the best pokemon! Build @version@, @link@",
+                          "@project@ (@channel@): We like a bit of Shinx too. Build @version@, @link@",
+                          "@project@ (@channel@): Build @version@ would like to thank their mother, father, and the great cube in the sky. @link@",
+                          "@project@ (@channel@): Prepare for the jarpocalypse! Build @version@, @link@",
+                          "@project@ (@channel@): Build @version@, a developer's best friend. @link@",
+                          "@project@ (@channel@): Once upon a time, there was a build @version@ that visited their grandma @link@",
+                          "@project@ (@channel@): Build @version@ Press any key to start. Well, where's the any key? @link@",
+                          "@project@ (@channel@): Build @version@ belongs to @link@. My precioussssss",
+                          "@project@ (@channel@): I LIKE TO MOVE IT MOVE IT. Build @version@, @link@"]
+    @@failure_messages = ["Build \#@build@ thought ice cream was better.",
+                          "Build \#@build@ walked the plank.",
+                          "Build \#@build@ fell off a cliff.",
+                          "Build \#@build@ stubbed its toe.",
+                          "Build \#@build@ walked the plank, yaarrr.",
+                          "Build \#@build@ was assassinated by pudding.",
+                          "Build \#@build@ has made a mess on the floor.",
+                          "Build \#@build@ was playing on the tracks.",
+                          "Build \#@build@ couldn't find the 'any' key.",
+                          "Build \#@build@ fell into a smeltery. Head first.",
+                          "Cleanup on aisle @build@..."]
 
     def initialize(bot)
       super bot
@@ -32,11 +69,37 @@ Daemons.run_proc('kalen.rb') do
         project = data['project']
         devchannel = data['channel']
         version = data['version']
+        url = Googl.shorten(data['url']).short_url
 
-        url = data['url']
-        puts channel.inspect
-        channel.msg "#{project}: #{devchannel} build #{version} pushed to Tanis: #{url}"
+        #channel.msg "#{project}: #{devchannel} build #{version} pushed to Tanis: #{url}"
+        channel.msg @@success_messages.sample.gsub("@project@", project).gsub("@channel@", devchannel).gsub("@version@", version).gsub("@link@", url)
         reportChangesForBuild(data)
+      rescue Exception => e
+        warn "Failed to send message: #{e.message}"
+      end
+    end
+
+    def report_start(data)
+      begin
+        channel = Channel(config[:channel])
+        project = data['project']
+        build = data['build']
+
+        msg = @@start_messages.sample.gsub("@project@", project).gsub("@build@", build)
+        channel.msg msg
+      rescue Exception => e
+        warn "Failed to send message: #{e.message}"
+      end
+    end
+
+    def report_fail(data)
+      begin
+        channel = Channel(config[:channel])
+        project = data['project']
+        build = data['build']
+
+        msg = @@failure_messages.sample.gsub("@project@", project).gsub("@build@", build)
+        channel.msg msg
       rescue Exception => e
         warn "Failed to send message: #{e.message}"
       end
@@ -48,6 +111,26 @@ Daemons.run_proc('kalen.rb') do
         data = MultiJson.decode payload
         info "Got POST from build endpoint: #{data.inspect}"
         report(data)
+      end
+      ret
+    end
+
+    def handle_start(req, payload)
+      ret = 200
+      begin
+        data = MultiJson.decode payload
+        info "Got POST from build-start endpoint: #{data.inspect}"
+        report_start(data)
+      end
+      ret
+    end
+
+    def handle_fail(req, payload)
+      ret = 200
+      begin
+        data = MultiJson.decode payload
+        info "Got POST from build-fail endpoint: #{data.inspect}"
+        report_fail(data)
       end
       ret
     end
@@ -99,6 +182,14 @@ Daemons.run_proc('kalen.rb') do
 
     post '/build' do
       settings.controller.handle(request, params[:payload])
+    end
+
+    post '/build-start' do
+      settings.controller.handle_start(request, params[:payload])
+    end
+
+    post '/build-fail' do
+      settings.controller.handle_fail(request, params[:payload])
     end
   end
 
